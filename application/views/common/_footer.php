434cd6117c606aba5db46f33a749cbc47a4517f7
Manishankar Vakta <evilrider555@gmail.com>
2020-10-02 23:18:38 +0600

    Last Update
    Testimonial and footer.

--- application/views/common/_footer.php
+++ application/views/common/_footer.php
@@ -1,3 +1,31 @@
+<style>
+  footer{
+    background: #333;
+    padding:25px 15px 0 15px;
+  }
+  footer, footer a{
+    color: #bbb;
+  }
+</style>
+<footer>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <p>&copy; 2020 All right Reserved</p>
+    </div>
+    <div class="col-md-6">
+      <p class="text-right">
+        <a href="#" class="social-link"><i class="fa fa-facebook-square fa-2x"></i></a>
+        <a href="#" class="social-link"><i class="fa fa-twitter-square fa-2x"></i></a>
+        <a href="#" class="social-link"><i class="fa fa-linkedin fa-2x"></i></a>
+        <a href="#" class="social-link"><i class="fa fa-whatsapp fa-2x"></i></a>
+      </p>
+    </div>
+  </div>
+
+</div>
+</footer>
+
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="<?php echo base_url('js/jquery-3.1.1.min.js'); ?>"></script>
--- application/views/common/_header.php
+++ application/views/common/_header.php
@@ -17,8 +17,13 @@
     <link href="css/style.css" rel="stylesheet">
     <script src="js/jquery.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js" ></script>
-    <script src="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+<script src="<?php echo base_url('js/jquery-3.1.1.min.js'); ?>"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
 
 
